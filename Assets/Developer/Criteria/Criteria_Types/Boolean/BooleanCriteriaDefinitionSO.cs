b25eb30fb1b0a650c71049229ffc805e17b8c2f0
melyunnie <meliwagner40@gmail.com>
2026-03-06 19:00:02 +0100

    Revert "Criteria/Achievement"
    This reverts commit fd8c3a198cdbf29e3d101d7a375f62faa625591c.

--- Assets/Developer/Criteria/Criteria_Types/Boolean/BooleanCriteriaDefinitionSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Achievements/Criteria/Boolean")]
-public class BooleanCriteriaDefinitionSO : ScriptableObject, ICriteriaDefinition
-{
-    public string id;
-    public string eventChannel;
-}
